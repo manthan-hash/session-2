5508925a344a528fb6a23e59b29a772361545f7c
Patel <M.Patel3363@student.leedsbeckett.ac.uk>
2025-02-05 14:09:56 +0000

    practice-3

---
+++ session-2prac-3.py
@@ -0,0 +1,8 @@
+name = input('Hello, what is your name? ')
+
+year_now = int(input('What year is it today? '))
+year_born = int(input("And what year you were born?\n"))
+
+age = year_now - year_born
+
+print(f"Hello,{name}. you will be {age} by today. ")
